66e04dfc41cd05265ea07ef9fdff43ddbb4e7b6f
Chelsea Becker <cschweig2@gmail.com>
2020-11-17 19:52:42 -0800

    refactor main and service files

---
+++ src/WeatherService.js
@@ -0,0 +1,15 @@
+// a service is a piece of reusable code that can be shared across an application
+
+export default class WeatherService {  
+  static async getWeather(city) {
+    try {
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,28 +2,37 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather-service.js'
+
+function clearFields() {
+  $('#location').val("");
+  $('.showErrors').text("");
+  $('.showHumidity').text("");
+  $('.showTemp').text("");
+}
+
+function getElements(response) {
+  if (response.main) {
+    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
+}
+
+// async function makeApiCall(city) {
+//   const response = await WeatherService.getWeather(city);
+//   getElements(response);
+// }
+// ANCHOR replaced by Immediately Invoked Function Expression below
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
-
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    }
-
-    request.open("GET", url, true);
-    request.send();
-
-   function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    }
+    let city = $('#location').val();
+    clearFields();
+    (async function() {
+      const response = await WeatherService.getWeather(city);
+      getElements(response);
+    })();
   });
 });
